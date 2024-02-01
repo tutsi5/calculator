fe5f94b18f1e09adc16bf2fc466df6659d3cf2cf
tutsi <tuta.matea@gmail.com>
2024-02-01 16:07:40 +0100

    Display numbers and operators on click

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let display = document.querySelector("#display")
+
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let zero = document.querySelector(".zero");
+
+let clearAll = document.querySelector(".clear-all");
+let del = document.querySelector(".delete");
+let addOperator = document.querySelector(".add");
+let subtractOperator =  document.querySelector(".subtract");
+let multiplyOperator = document.querySelector(".multiply");
+let divideOperator = document.querySelector(".divide");
+let equals = document.querySelector(".equals");
+let dot = document.querySelector(".dot");
+
 function add(a, b) {
     return a+b;
 }
@@ -14,9 +36,9 @@ function divide(a, b) {
     return a/b;
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = "";
+let currentOperator = "";
+let secondNumber = "";
 
 function operate(firstNumber, operator, secondNumber) {
     if (operator == "+") {
@@ -29,3 +51,103 @@ function operate(firstNumber, operator, secondNumber) {
         return divide(firstNumber, secondNumber);
     }
 }
+
+let isThereOperator = false;
+
+function displayOperator(operator) {
+    if (isThereOperator == false) {
+        let newOperator = document.createTextNode(operator);
+        display.appendChild(newOperator);
+        currentOperator = operator;
+        isThereOperator = true;
+    } else if(operator != currentOperator) {
+        display.removeChild(display.lastChild);
+        let newOperator = document.createTextNode(operator);
+        display.appendChild(newOperator);
+        currentOperator = operator;
+    }
+}
+
+function addNumber(number) {
+    let nextNumber = document.createTextNode(number);
+    display.appendChild(nextNumber);
+}
+
+let checkIfSecondNumber = false;
+function checkNumberPosition(number) {
+    if (checkIfSecondNumber == true) {
+        secondNumber += number;
+    } else {
+        firstNumber += number;
+    }
+}
+
+//Numbers
+one.addEventListener("click", function(){
+    addNumber("1");
+    checkNumberPosition("1");
+})
+
+two.addEventListener("click", function(){
+    addNumber("2");
+    checkNumberPosition("2");
+})
+
+three.addEventListener("click", function(){
+    addNumber("3");
+    checkNumberPosition("3");
+})
+
+four.addEventListener("click", function(){
+    addNumber("4");
+    checkNumberPosition("4");
+})
+
+five.addEventListener("click", function(){
+    addNumber("5");
+    checkNumberPosition("5");
+})
+
+six.addEventListener("click", function(){
+    addNumber("6");
+    checkNumberPosition("6");
+})
+
+seven.addEventListener("click", function(){
+    addNumber("7");
+    checkNumberPosition("7");
+})
+
+eight.addEventListener("click", function(){
+    addNumber("8");
+    checkNumberPosition("8");
+})
+
+nine.addEventListener("click", function(){
+    addNumber("9");
+    checkNumberPosition("9");
+})
+
+zero.addEventListener("click", function(){
+    addNumber("0");
+    checkNumberPosition("0");
+})
+
+//Operators and deletion
+addOperator.addEventListener("click", function(){
+    displayOperator("+");
+})
+
+subtractOperator.addEventListener("click", function(){
+    displayOperator("-");
+})
+
+multiplyOperator.addEventListener("click", function(){
+    displayOperator("x");
+})
+
+divideOperator.addEventListener("click", function(){
+    displayOperator("%");
+})
+
+
